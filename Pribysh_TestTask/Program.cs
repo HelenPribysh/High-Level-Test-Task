5058d498215591c703f2a726eb485fcecf6cb2ce
he11en_ <helenpribysh@gmail.com>
2020-12-19 20:31:25 +0300

    TestTask

--- Pribysh_TestTask/Program.cs
+++ Pribysh_TestTask/Program.cs
@@ -6,7 +6,34 @@ namespace Pribysh_TestTask
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = { 1, 2, 3, 4, 5 };
+            ReturnSumArray(arr);
+        }
+
+        static int[] ReturnSumArray (int[] arr)
+        {
+            try
+            {
+                int count = 0;
+                int[] sumArr = new int[arr.Length];
+                for (int i = 0; i < arr.Length; i++)
+                {                    
+                    sumArr[i] = count + arr[i];
+                    count += arr[i];
+                }
+
+                foreach (var item in sumArr)
+                {
+                    Console.WriteLine(item);
+                }
+
+                return sumArr;                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new int[arr.Length];
+            }
         }
     }
 }
